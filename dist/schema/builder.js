1904940483d830ef4f7146d97966d4ec1d120a80
Harrison Fornasier <harrisonfornasier@gmail.com>
2025-05-13 12:00:32 +0100

    Remove obsolete dist folder

--- dist/schema/builder.js
+++
@@ -1,4 +0,0 @@
-import SchemaBuilder from "@pothos/core";
-import { DateResolver } from "graphql-scalars";
-export const builder = new SchemaBuilder({});
-builder.addScalarType("Date", DateResolver, {});
